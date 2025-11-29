efe706d5de91b461cd0ec56b20062b38734d0c4e
Oscar Ramírez <143234112+Lconejo@users.noreply.github.com>
2025-11-29 19:05:06 +0000

    Agregué el excel reader

---
+++ src/excel_reader.py
@@ -0,0 +1,24 @@
+#En este archivo se abre el excel con las operaciones y se manejan los datos
+import openpyxl 
+from interfaz import path #supongamos que ya tenemos el path
+#Abriremos el archivo usando el Path que se solicitó en la interfaz
+
+def excelReader(path : str) -> list:
+
+    contenido = [] #Aquí guardaremos las expresiones de la celda
+
+    try:
+        workBook = openpyxl.load_workbook(path)
+        sheet = workBook.active
+    
+    except Exception as e:
+        print(f"Error al intentar abrir el archivo . . .: \n {e}")
+        return []
+
+    for row in sheet.iter_rows(min_row=1, max_col=1):
+        for cell in row:
+            if cell.value is not None:
+                contenido.append(cell.value)
+    
+    workBook.close()
+    return contenido #el tokenizador y parser ya pueden llamar al método
