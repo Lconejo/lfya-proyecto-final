6343236cb198fcf6e6470308403db37d4ccbdeea
chralondra <143234748+alondrachavezramirez11@users.noreply.github.com>
2025-11-30 21:40:33 -0600

    Update lexer.py

--- src/operations/lexer.py
+++ src/operations/lexer.py
@@ -1,5 +1,6 @@
 #Lexer con Ply
 import ply.lex as lex
+
 #      LISTA DE TOKENS
 tokens = (
     'NUMBER',
@@ -11,15 +12,16 @@ tokens = (
     'RPAREN',
     'ABS',
     'SQRT',
-    'EXP',
-    'ID'
+    'EXP'
 )
-#   PALABRAS RESERVADAS
+
+#   PALABRAS RESERVADAS 
 reserved = {
     'abs': 'ABS',
     'sqrt': 'SQRT',
     'exp': 'EXP'
 }
+
 #     TOKENS SIMPLES
 t_PLUS   = r'\+'
 t_MINUS  = r'-'
@@ -27,22 +29,31 @@ t_TIMES  = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+
 #    NÚMEROS 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
 #    IDENTIFICADORES
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     # Verificamos si es palabra reservada 
     t.type = reserved.get(t.value, 'ID')
     return t
-  #     IGNORAR ESPACIOS
+
+#     IGNORAR ESPACIOS
+
 t_ignore = ' \t'
+
+
 #     MANEJO DE ERRORES
+
 def t_error(t):
     print("Carácter ilegal:", t.value[0])
     t.lexer.skip(1)
+
 #     CONSTRUIR EL LEXER
+
 lexer = lex.lex()
